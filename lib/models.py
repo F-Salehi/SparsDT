1215b5381de535380f2412df4dd2acd4f756ab9a
F-Salehi <farnood.salehi@epfl.ch>
2019-05-05 18:18:29 +0200

    Implemented the fit function

--- lib/models.py
+++ lib/models.py
@@ -32,34 +32,39 @@ class ModelSparseDT:
             raise ValueError("Y must be a matrix of dimention 2 with K observation")
         self.n_cols = n_cols
         self.m_rows = Y.size(0)
-        self.K = Y.size(1)
+        self.K = Y.size(1) # Number of observations
         self.device = device
         ### Setting A
-        self.A = A if A is not None else torch.rand([self.m_rows,self.n_cols],dtype=torch.float32, requires_grad=True)
+        self.A = A if A is not None else torch.rand([self.m_rows,self.n_cols],dtype=torch.float32)
         self.A = self.A / self.A.data.norm(dim=0)
-        self.A = self.A.to(self.device)
+        self.A = self.A.to(self.device).requires_grad_()
         self.Y = Y.to(self.device)
         self.batch = batch
-        self.alpha = alpha if alpha is not None else self.estim_alpha(num_samples)
+        self.alpha = alpha if alpha is not None else self._estim_alpha(num_samples)
         self.p = 0.4
         self.c = (2**(self.p+1)) * func.gamma((self.p+1)/2.0) * \
             func.gamma(-self.p/self.alpha) /(self.alpha*(np.pi**0.5) * func.gamma(-self.p/2.0) )
 
-    def set_optimizer(self, optim, lr=0.1, ):
-        if optim == 'Adagrad':
-            self.optimizer = optim.Adagrad([self.A], lr=lr)
-        elif optim == 'SGD':
-            self.optimizer = optim.SGD([self.A], lr=lr)
-        else:
-            raise ValueError("Unknown optimizer")
 
-    def estim_alpha(self, num_samples):
+    def set_optimizer(self, name_optim, args):
+        '''
+        Setting the optimizer
+        
+        Arguments:
+        
+        name_optim: name of the optimizer (string)
+        args: arguments of the optimizer (dict)
+        '''
+        self.optimizer = getattr(optim, name_optim)([self.A], **args)
+
+
+    def _estim_alpha(self, num_samples):
         """
         Estimating the alpha using the algorithm in
         "Reconstruction of Ultrasound RF Echoes Modeled as Stable Random Variables"
         returns alpha_hat
         """
-        print('============ estimating alpha ================')
+        print('================= estimating alpha =================')
         alphahat = 0
         count = 0
         while count <= num_samples:
@@ -73,28 +78,25 @@ class ModelSparseDT:
             alpha = alpha_sq ** 0.5
             alphahat += alpha.sum() 
             count += self.batch
-            print(f"Initialize estimating {100 * round(min((count/num_samples),1),1)} | "
-                f"estimated_alpha: {alphahat/count:.2e}", end="\r")
+            print(f"Initialize estimation {100 * round(min((count/num_samples),1),1)}% | "
+                f"estimated_alpha: {alphahat/count:.2f}", end="\r")
         alpha = alphahat/count
         return alpha
 
-    def _estimate_gamma(self, alpha, U):
+    def _estimate_gamma(self, U):
         """
         estimating the dispersion gamma for the given U and Y
-        alpha: int
-            estimated alpha
         U: torch.tensor
             A random U
         """
-        p = 0.4
         UT = U.transpose(1,0)
-        h = UT.mm(Y).abs()**self.p
+        h = UT.mm(self.Y).abs()**self.p
         EXP = h.mean(dim=1)
         gamma = (EXP/self.c)**(1/self.p)
         return gamma
 
 
-    def findU(self, size):
+    def _findU(self, size):
         """
         Finding U such that their estimated alpha for U*Y is close to the given alpha of the dataset
         size : int
@@ -111,20 +113,52 @@ class ModelSparseDT:
             alpha_sq = (2 * np.pi**2 /(12*k2*(1 - np.pi**2/(12*k2))))
             idx =  (self.alpha**2-0.2<alpha_sq) * (alpha_sq<self.alpha**2+0.2)
             U = UT[idx,:].transpose(1,0) 
-            Uall = torch.cat([A,C],dim=1)
+            Uall = torch.cat([Uall,U],dim=1)
             count += U.size(1)
         return Uall
 
-    def norm_alpha(self, U):
-        AT = self.A.transpose(1,0)
-        B = AT.mm(U).abs()**alpha
+    def _norm_alpha(self, A, U):
+        AT = A.transpose(1,0)
+        B = AT.mm(U).abs()**self.alpha
         norm = B.sum(dim=0)
         return norm
 
-    def Loss(self, U, gamma, alpha):
-        norm = norm_alpha(A,U,alpha)
-        cost = ((norm-gamma**alpha) **2).mean()
+    def Loss(self, U, gamma):
+        norm = self._norm_alpha(self.A, U)
+        cost = ((norm-gamma**self.alpha) **2).mean()
         return cost
+
+    def fit(self, max_iter=1e4, rand_U=False, num_col = None):
+        '''
+        Fitting the model
+
+        -----------------------
+        Arguments:
+        max_iter ; int
+            maximum number of iterations
+        rand_U : boolian
+            specify if U are selected randomly or found such that 
+            their estimated alpha is close to the initial estimation
+        num_col : int
+            Number of columns of U
+
+        '''
+        num_col = num_col if num_col else  2*self.m_rows*self.n_cols
+
+        for it in range(int(max_iter)):
+            U = self._findU(num_col) if rand_U else torch.randn([self.m_rows, num_col], device=self.device)
+            gamma = self._estimate_gamma(U)
+            
+            self.optimizer.zero_grad()
+            loss = self.Loss(U, gamma)
+            loss.backward()
+            self.optimizer.step()
+
+            # Check that the optimization did not fail
+            if torch.isnan(self.A).any():
+                raise ValueError('NaNs in coeffs! Stop optimization...')
+
+            return self.A.data
 
 
 
